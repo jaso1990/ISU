c3a4e23505dbb4c8acd1be23a4a4312af6640be2
jason <jason@localhost>
2020-01-13 10:39:13 -0500

    worked at home

--- src/Clothing.java
+++ src/Clothing.java
@@ -3,8 +3,9 @@ public class Clothing extends Item{
     
     private String size; //clothing will have a size (S, M, L, XL)
     
-    public Clothing(String n, double c, int q, String s ){
-        super (n, c, q);
+    //this constructor will include a size when creating object
+    public Clothing(String n, double c, int q, String t, String s){
+        super (n, c, q, t);
         size = s;
     }
     
@@ -16,7 +17,7 @@ public class Clothing extends Item{
     @Override
     public double total() {
         double total = cost * quant;
-        totalRetailCost += total;
+        
         return total;
     }
     
